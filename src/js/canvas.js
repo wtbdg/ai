a743e2ea708732861a7ea5d521225ea2069ddadc
Rio Purnomo <siriokun@gmail.com>
2022-01-23 18:11:43 +0700

    center frame

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -16,7 +16,7 @@ const SETTINGS = {
   strokeStyle: 'red',
   rotationOffsetX: 0, // negative -> look upper. in radians
   cameraFOV: 40,    // in degrees, 3D camera FOV
-  pivotOffsetYZ: [0.5, 0.2], // XYZ of the distance between the center of the cube and the pivot
+  pivotOffsetYZ: [-0.5, 0.2], // XYZ of the distance between the center of the cube and the pivot
   detectionThreshold: 0.75, // sensibility, between 0 and 1. Less -> more sensitive
   detectionHysteresis: 0.05,
   scale: [1.0, 1.0], // scale of the 2D canvas along horizontal and vertical 2D axis
